6c43c5afc12bf1ff14d895c243e6ecd2be061a21
aaaaabbbbb <nomovl@seznam.cz>
2020-05-01 01:52:32 +0200

    jouuu

--- testest/Program.cs
+++ testest/Program.cs
@@ -10,90 +10,59 @@ namespace testest
     {
         static void Main(string[] args)
         {
-            string[] support = { "Furia", "Seris", "Grover", "Ying", "Jenos", "Mal'Damba", "Io", "Grohk", "Pip" };
+            string[] support = { "Furia", "Seris", "Grover", "Ying", "Jenos", "Mal'Damba", "Io", "Grohk", "Pip", "Corvus" };
             string[] flanker = { "Androxus", "Buck", "Evie", "Koga", "Lex", "Maeve", "Moji", "Skye", "Talus", "Zhin" };
             string[] tank = { "Ash", "Atlas", "Barik", "Fernando", "Inara", "Khan", "Makoa", "Raum", "Ruckus", "Terminus", "Torvald (I´m so sorry)" };
             string[] damage = { "Bomb king", "Cassie", "Dredge", "Drogoz", "Imani", "Kinessa", "Lian", "Shalin", "Strix", "Tiberius", "Tyra", "Viktor", "Vivian", "Willo" };
             string[] all = support.Concat(flanker.Concat(tank.Concat(damage))).ToArray();
             Random rnd = new Random();
-            int SupLen = support.Length-1;
-            int FlaLen = flanker.Length-1;
-            int TanLen = tank.Length-1;
-            int DamLen = damage.Length-1;
-            int AllLen = all.Length-1;
             Console.WriteLine("Choose your class: Support (s), Flank(f), Tank(t), Damage(d) or all of them (all)");
             string Class = Console.ReadLine();
             if (Class.Contains("+"))
             {
-                string[] Brambora = new string[100];
+                string[] Brambora = new string[(Math.Max(Math.Max(support.Length, flanker.Length), Math.Max (tank.Length, damage.Length)))*11];
+                Brambora = ClassOne(Class, support, flanker, tank, damage, 0);
                 int Loong = Class.Length;
-                switch (Loong)
+                int Petr = 0;
+                while (Petr != Loong-1)
                 {
-                    case 3:
-                        Brambora = ClassOne(Class, support, flanker, tank, damage, 0).Concat(ClassOne(Class, support, flanker, tank, damage, 2)).ToArray();
-                        break;
-                    case 5:
-                        Brambora = ClassOne(Class, support, flanker, tank, damage, 0).Concat(ClassOne(Class, support, flanker, tank, damage, 2).Concat(ClassOne(Class, support, flanker, tank, damage, 4))).ToArray();
-                        break;
-                    case 7:
-                        Brambora = ClassOne(Class, support, flanker, tank, damage, 0).Concat(ClassOne(Class, support, flanker, tank, damage, 2).Concat(ClassOne(Class, support, flanker, tank, damage, 4)).Concat(ClassOne(Class, support, flanker, tank, damage, 6))).ToArray();
-                        break;
-                    case 9:
-                        Brambora = ClassOne(Class, support, flanker, tank, damage, 0).Concat(ClassOne(Class, support, flanker, tank, damage, 2).Concat(ClassOne(Class, support, flanker, tank, damage, 4)).Concat(ClassOne(Class, support, flanker, tank, damage, 6)).Concat(ClassOne(Class, support, flanker, tank, damage, 8))).ToArray();
-                        break;
-                    case 11:
-                        Brambora = ClassOne(Class, support, flanker, tank, damage, 0).Concat(ClassOne(Class, support, flanker, tank, damage, 2).Concat(ClassOne(Class, support, flanker, tank, damage, 4)).Concat(ClassOne(Class, support, flanker, tank, damage, 6)).Concat(ClassOne(Class, support, flanker, tank, damage, 8)).Concat(ClassOne(Class, support, flanker, tank, damage, 10))).ToArray();
-                        break;
-                    case 13:
-                        Brambora = ClassOne(Class, support, flanker, tank, damage, 0).Concat(ClassOne(Class, support, flanker, tank, damage, 2).Concat(ClassOne(Class, support, flanker, tank, damage, 4)).Concat(ClassOne(Class, support, flanker, tank, damage, 6)).Concat(ClassOne(Class, support, flanker, tank, damage, 8)).Concat(ClassOne(Class, support, flanker, tank, damage, 10)).Concat(ClassOne(Class, support, flanker, tank, damage, 12))).ToArray();
-                        break;
-                    case 15:
-                        Brambora = ClassOne(Class, support, flanker, tank, damage, 0).Concat(ClassOne(Class, support, flanker, tank, damage, 2).Concat(ClassOne(Class, support, flanker, tank, damage, 4)).Concat(ClassOne(Class, support, flanker, tank, damage, 6)).Concat(ClassOne(Class, support, flanker, tank, damage, 8)).Concat(ClassOne(Class, support, flanker, tank, damage, 10)).Concat(ClassOne(Class, support, flanker, tank, damage, 12)).Concat(ClassOne(Class, support, flanker, tank, damage, 14))).ToArray();
-                        break;
-                    case 17:
-                        Brambora = ClassOne(Class, support, flanker, tank, damage, 0).Concat(ClassOne(Class, support, flanker, tank, damage, 2).Concat(ClassOne(Class, support, flanker, tank, damage, 4)).Concat(ClassOne(Class, support, flanker, tank, damage, 6)).Concat(ClassOne(Class, support, flanker, tank, damage, 8)).Concat(ClassOne(Class, support, flanker, tank, damage, 10)).Concat(ClassOne(Class, support, flanker, tank, damage, 12)).Concat(ClassOne(Class, support, flanker, tank, damage, 14)).Concat(ClassOne(Class, support, flanker, tank, damage, 16))).ToArray();
-                        break;
-                    case 19:
-                        Brambora = ClassOne(Class, support, flanker, tank, damage, 0).Concat(ClassOne(Class, support, flanker, tank, damage, 2).Concat(ClassOne(Class, support, flanker, tank, damage, 4)).Concat(ClassOne(Class, support, flanker, tank, damage, 6)).Concat(ClassOne(Class, support, flanker, tank, damage, 8)).Concat(ClassOne(Class, support, flanker, tank, damage, 10)).Concat(ClassOne(Class, support, flanker, tank, damage, 12)).Concat(ClassOne(Class, support, flanker, tank, damage, 14)).Concat(ClassOne(Class, support, flanker, tank, damage, 16)).Concat(ClassOne(Class, support, flanker, tank, damage, 18))).ToArray();
-                        break;
-                    case 21:
-                        Brambora = ClassOne(Class, support, flanker, tank, damage, 0).Concat(ClassOne(Class, support, flanker, tank, damage, 2).Concat(ClassOne(Class, support, flanker, tank, damage, 4)).Concat(ClassOne(Class, support, flanker, tank, damage, 6)).Concat(ClassOne(Class, support, flanker, tank, damage, 8)).Concat(ClassOne(Class, support, flanker, tank, damage, 10)).Concat(ClassOne(Class, support, flanker, tank, damage, 12)).Concat(ClassOne(Class, support, flanker, tank, damage, 14)).Concat(ClassOne(Class, support, flanker, tank, damage, 16)).Concat(ClassOne(Class, support, flanker, tank, damage, 18)).Concat(ClassOne(Class, support, flanker, tank, damage, 18))).ToArray();
-                        break;
+                    Brambora= Brambora.Concat(ClassOne(Class, support, flanker, tank, damage, Petr)).ToArray();
+                    Petr += 2;
                 }
-                int num69 = Brambora.Length-1;
-                int cislo = rnd.Next(0, num69);
-                Console.WriteLine(Brambora[cislo]);
+                int cislo69 = rnd.Next(Brambora.Length);
+                Console.WriteLine(Brambora[cislo69]);               
+
             }
             else
             {
                 if (Class.ToLower().Contains("all"))
                 {
 
-                    int cislo = rnd.Next(0, AllLen);
+                    int cislo = rnd.Next(all.Length);
                     Console.WriteLine(all[cislo]);
                 }
                 if (Class.ToLower().Contains("s"))
                 {
                     Console.WriteLine("");
-                    int cislo = rnd.Next(0, SupLen);
+                    int cislo = rnd.Next(support.Length);
                     Console.WriteLine(support[cislo]);
                 }
                 if (Class.ToLower().Contains("f"))
                 {
                     Console.WriteLine("");
-                    int cislo = rnd.Next(0, FlaLen);
+                    int cislo = rnd.Next(flanker.Length);
                     Console.WriteLine(flanker[cislo]);
                 }
                 if (Class.ToLower().Contains("t") || Class.ToLower().Contains("tank") || Class.ToLower().Contains("front"))
                 {
                     Console.WriteLine("");
-                    int cislo = rnd.Next(0, TanLen);
+                    int cislo = rnd.Next(tank.Length);
                     Console.WriteLine(tank[cislo]);
                 }
                 if (Class.ToLower().Contains("d") || Class.ToLower().Contains("dam") || Class.ToLower().Contains("dmg"))
                 {
                     Console.WriteLine("");
-                    int cislo = rnd.Next(0, DamLen);
+                    int cislo = rnd.Next(damage.Length);
                     Console.WriteLine(damage[cislo]);
                 }
             }
